0d5db2ee670e7790a28ce3de25f72ef5c00fd6e3
t0yman <195047463+t0yman@users.noreply.github.com>
2025-10-22 17:11:10 -0700

    updated DrawFilledTriangle demo

--- source/main.cpp
+++ source/main.cpp
@@ -5,44 +5,58 @@
 
 int main()
 {
-    const int width = 600;
-    const int height = 600;
+    const int width = 800;
+    const int height = 800;
 
     PpmImage ppmImage{width, height};
 
     ppmImage.DrawFilledTriangle(
-        Point2{50.0, 50.0},
-        Point2{200.0, 50.0},
-        Point2{100.0, 400.0},
-        Color{1.0, 0.0, 0.0}
+        Point2{100.0, 100.0},
+        Point2{700.0, 150.0},
+        Point2{400.0, 650.0},
+        Color{0.8, 0.1, 0.1}
     );
 
     ppmImage.DrawFilledTriangle(
-        Point2{450.0, 50.0},
-        Point2{300.0, 200.0},
-        Point2{400.0, 400.0},
-        Color{0.0, 1.0, 0.0}
+        Point2{150.0, 200.0},
+        Point2{350.0, 180.0},
+        Point2{250.0, 500.0},
+        Color{0.1, 0.3, 0.8}
     );
 
     ppmImage.DrawFilledTriangle(
-        Point2{500.0, 450.0},
-        Point2{520.0, 480.0},
-        Point2{550.0, 580.0},
-        Color{1.0, 1.0, 0.0}
+        Point2{500.0, 250.0},
+        Point2{650.0, 280.0},
+        Point2{580.0, 550.0},
+        Color{0.2, 0.7, 0.2}
     );
 
     ppmImage.DrawFilledTriangle(
-        Point2{50.0, 450.0},
-        Point2{80.0, 550.0},
-        Point2{100.0, 580.0},
-        Color{1.0, 0.0, 1.0}
+        Point2{200.0, 500.0},
+        Point2{350.0, 520.0},
+        Point2{275.0, 700.0},
+        Color{0.9, 0.9, 0.1}
     );
 
     ppmImage.DrawFilledTriangle(
-        Point2{350.0, 450.0},
-        Point2{420.0, 500.0},
-        Point2{380.0, 580.0},
-        Color{0.0, 1.0, 1.0}
+        Point2{450.0, 400.0},
+        Point2{600.0, 450.0},
+        Point2{520.0, 650.0},
+        Color{0.7, 0.2, 0.7}
+    );
+
+    ppmImage.DrawFilledTriangle(
+        Point2{600.0, 100.0},
+        Point2{750.0, 150.0},
+        Point2{680.0, 350.0},
+        Color{0.2, 0.8, 0.8}
+    );
+
+    ppmImage.DrawFilledTriangle(
+        Point2{50.0, 600.0},
+        Point2{200.0, 650.0},
+        Point2{120.0, 750.0},
+        Color{0.9, 0.5, 0.1}
     );
 
     ppmImage.Write("output");
